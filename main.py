eef16d122beb2088053a5ffe14fd079e2ba25af7
Sergey <Sergey@localhost.localdomain>
2022-09-29 18:42:40 +0300

    Made constructing authentification url and function for getting list of groups

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+from urllib.parse import urlparse, urlencode, urlunparse
 import requests
+from dotenv import load_dotenv
 
 
 def download_image(url, path, params=None):
@@ -22,8 +25,31 @@ def get_image_comment(base_url):
     return image_details['alt']
 
 
+def make_url_for_token(client_id):
+    url = 'https://oauth.vk.com/authorize'
+    params = {
+        'client_id': client_id,
+        'scope': 'scope=photos,groups',
+        'response_type': 'token'
+    }
+    url_parts = urlparse(url)
+    url_parts = url_parts._replace(query=urlencode(params))
+    return urlunparse(url_parts)
+
+
+def get_user_groups(user_id, token):
+    url = 'https://api.vk.com/method/groups.get'
+    params = {'access_token': token, 'user_id': user_id, 'v': '5.194'}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    image_comment = get_image_comment('https://xkcd.com/353/')
-    print(image_comment)
+    load_dotenv()
+    user_id = os.getenv('USER_ID')
+    token = os.getenv('ACCESS_TOKEN')
+    print(get_user_groups(user_id, token))
+
 
